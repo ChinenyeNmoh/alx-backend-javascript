39832b80ca7584c000f4e5f20a02a50d1cf67766
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-11-29 23:23:54 +0100

    promise

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,12 +1,8 @@
 /* eslint-disable-next-line */
 export default function divideFunction(numerator, denominator) {
   try {
-    if (denominator === 0) {
-      throw (Error('cannot divide by 0'));
-    } else {
-      return numerator / denominator;
-    }
+    if (denominator === 0) { throw new Error('cannot divide by 0'); }
+    return numerator / denominator;
   } catch (error) {
-    console.log(error);
-  }
+    throw new Error('cannot divide by 0');
 }
