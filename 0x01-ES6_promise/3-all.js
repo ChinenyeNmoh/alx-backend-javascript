1882ae2af13bd09ab5afce679f20a700be45bd53
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-11-29 20:56:52 +0100

    promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   const p1 = Promise.resolve(uploadPhoto());
   const p2 = Promise.resolve(createUser());
   Promise.all([p1, p2]).then((resArray) => {
-    console.log(resArray[0].body, resArray[1].firstName, resArray[1].lastName);
+    console.log(`${resArray[0].body} ${resArray[1].firstName} ${resArray[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });
 }
