1133d7c534b0fceb5611fb3141a0632fae64125c
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-11-29 10:05:50 +0100

    arrowfunction

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/* task 2 arrow function */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
