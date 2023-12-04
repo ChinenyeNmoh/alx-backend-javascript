882627706cc3df3c15e6910b1273570b87968e72
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-12-04 18:34:25 +0100

    data-manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   const newarr = [...set];
   let str = '';
-  if (Startstring === undefined || startString.length === 0) {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
   newarr.forEach((word) => {
     if (word.startsWith(startString)) {
-      str += `${word.slice(3)}-`;
+      str += `${word.slice(startString.length)}-`; // Updated to use startString.length
     }
   });
 
