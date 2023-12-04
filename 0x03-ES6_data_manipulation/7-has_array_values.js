c3a81cec21ef161e60541f7c77b0300bcd77b1d0
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-12-04 18:11:22 +0100

    data-manipulation

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,6 +2,3 @@ export default function hasValuesFromArray(set, array) {
   const res = array.every((item) => set.has(item));
   return res;
 }
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
