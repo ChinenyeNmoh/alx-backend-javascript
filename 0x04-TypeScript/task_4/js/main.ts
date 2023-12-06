17bbde7ff47941bb745ff4b3accc1f5d827c0bf4
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-12-06 18:17:13 +0100

    TS

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,8 +2,8 @@ export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-    firstName: 'Abdul',
-    lastName: 'Azindo',
+    firstName: 'Osinachi',
+    lastName: 'Nmoh',
     experienceTeachingC: 10,
 }
 
